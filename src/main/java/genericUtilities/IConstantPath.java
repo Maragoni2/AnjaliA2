2fd3c667bdaf6bd7da507e30b9ab8074805c5b3b
anjal <anjal@ANJALI>
2024-02-03 12:44:34 +0530

    Framework A2 first commit

---
+++ src/main/java/genericUtilities/IConstantPath.java
@@ -0,0 +1,13 @@
+package genericUtilities;
+
+/**
+ * This interface contains the path of external files like properties and excel
+ * @author
+ */
+
+public interface IConstantPath {
+	String  PROPERTIES_FILE_PATH="./src/test/resources/commondata.properties";
+	String Excel_PATH = "./src/test/resources/Testdata.xlsx";
+	String Excel_PATH_ = null;
+
+}
